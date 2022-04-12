7b278a39315373522ef112bb0cbd5f2f26296cf3
pavel <shirokuiuna@mail.ru>
2022-04-12 17:00:32 +0300

    Подключил стили, шрифты и картинки

--- src/store/main-page-process/reducer/main-page-process.ts
+++ src/store/main-page-process/reducer/main-page-process.ts
@@ -1,21 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { InitialState } from 'src/store/types/main-page-process';
 import { NameSpace } from 'src/store/constants';
-import { ActionType } from 'src/store/main-page-process/action-type';
 
-const initialState: InitialState = {
-  title: 'Hello',
-};
+const initialState = {};
 
 export const mainPageProcess = createSlice({
   name: NameSpace.MainPage,
   initialState,
-  reducers: {
-    [ActionType.ClearState](state) {
-      state.title = 'State is cleared';
-    },
-  },
+  reducers: {},
 });
 
-export const { clearState } = mainPageProcess.actions;
+export const {} = mainPageProcess.actions;
--- src/store/main-page-process/selectors.ts
+++ src/store/main-page-process/selectors.ts
@@ -1,4 +1,1 @@
-import { NameSpace } from 'src/store/constants';
-import { State } from 'src/store/types/state';
-
-export const getTitle = (state: State) => state[NameSpace.MainPage].title;
+export const empty = {};
