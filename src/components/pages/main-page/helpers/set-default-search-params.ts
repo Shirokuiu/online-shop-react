e09dcf8bb40837f80454eb173ebb5f432205e670
pavelosipov <osipov_p_v@ntik.ru>
2022-05-03 08:08:00 +0300

    Удалил ненужный код

--- src/components/pages/main-page/helpers/set-default-search-params.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultSearchParamsType } from 'src/components/pages/main-page/types/main-page-filter-categories';
-import { CategoryType } from 'src/store/types/main-page-process';
-
-export const setDefaultSearchParams = (): DefaultSearchParamsType => ({
-  category: CategoryType.All,
-});
